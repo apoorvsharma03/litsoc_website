ed27da87959c34fcaf88b540bf7b4217d6d81f62
Apoorv <apoorvsharma2603@gmail.com>
2023-08-20 14:24:09 +0530

    Added Sample Main

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,67 @@ const App = () => {
   return (
     <>
       <Header/>
-      <main>
+      <main className='mt-[50px] lg:mt-[100px]'>
+        <section id='about_us'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>ABOUT US</h1>
+            <div className='bg-[#302306]'>
+              <img src={About_bg} alt="about_us" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
+        <section id='reviews'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>REVIEWS</h1>
+            <div className='bg-[#302306]'>
+              <img src={Review_jpg} alt="reviews" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
+        <section id='compositions'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>COMPOSITIONS</h1>
+            <div className='bg-[#302306]'>
+              <img src={Compositions_bg} alt="compositions" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
+        <section id='gallery'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>GALLERY</h1>
+            <div className='bg-[#302306]'>
+              <img src={Gallery_bg} alt="gallery" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
+        <section id='achievements'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>ACHIEVEMENTS</h1>
+            <div className='bg-[#302306]'>
+              <img src={Achievements_bg} alt="achievements" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
+        <section id='team'>
+          <div className='w-fit h-fit'>
+            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>TEAM</h1>
+            <div className='bg-[#302306]'>
+              <img src={Team_bg} alt="team" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
+            </div>
+          </div>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
+          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        </section>
       </main>
       <Footer/>
     </>
