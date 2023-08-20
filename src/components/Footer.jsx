5f486f8d202ab258b06afd61583de7601e8f5c4c
Apoorv <apoorvsharma2603@gmail.com>
2023-08-20 14:31:57 +0530

    minor delay on hover added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
       <h1 className='text-[#222222d4] text-lg md:text-2xl'>Contact Us</h1>
       <hr className='border-[#222222d4] mt-2 mb-5'/>
       <div className='flex justify-around flex-wrap lg:flex-nowrap gap-x-14 gap-y-5 md:gap-y-10'>
-        <div className='flex justify-center items-center gap-2 hover:text-[#25863f]'>
+        <div className='flex justify-center items-center gap-2 duration-100 hover:text-[#25863f]'>
           <div className='opacity-70 text-3xl md:text-4xl  flex items-center justify-center'>
             <ion-icon name="call"></ion-icon>
           </div>
-          <div className='flex items-center gap-2'>
+          <div className='flex items-center gap-2 duration-75'>
             <div>
               <h2>Sparsh:<div>1234567890</div></h2>              
             </div>
@@ -20,7 +20,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <a href='mailto:litsoc.bitm@gmail.com' className='flex justify-center items-center gap-2 hover:text-[#913326]'>
+        <a href='mailto:litsoc.bitm@gmail.com' className='flex justify-center items-center gap-2 duration-100 hover:text-[#913326]'>
           <div className='opacity-70 text-3xl md:text-4xl flex items-center justify-center'>
             <ion-icon name="mail"></ion-icon>
           </div>
@@ -29,7 +29,7 @@ const Footer = () => {
           </div>
         </a>
         <div className='flex justify-center gap-5 xl:gap-0  xl:justify-between w-full'>
-          <a href='https://www.instagram.com/litsocbitm/' className='flex justify-center items-center gap-2 hover:text-[#E1306C]'>
+          <a href='https://www.instagram.com/litsocbitm/' className='flex justify-center items-center gap-2 duration-100 hover:text-[#E1306C]'>
             <div className='opacity-70 text-3xl md:text-4xl flex items-center justify-center'>
               <ion-icon name="logo-instagram"></ion-icon>
             </div>
@@ -38,7 +38,7 @@ const Footer = () => {
               <p>litsocbitm</p>
             </div>
           </a>
-          <a href='https://www.facebook.com/LiterarySocietyBITMesra' className='flex justify-center items-center gap-2 hover:text-[#1876f2e9]'>
+          <a href='https://www.facebook.com/LiterarySocietyBITMesra' className='flex justify-center items-center gap-2 duration-100 hover:text-[#1876f2e9]'>
             <div className='opacity-70 text-3xl md:text-4xl flex items-center justify-center'>
               <ion-icon name="logo-facebook"></ion-icon>
             </div>
@@ -47,7 +47,7 @@ const Footer = () => {
               <p>Literary Society, BIT Mesra</p>
             </div>
           </a>
-          <a href='https://www.linkedin.com/company/literary-society-bit-mesra/mycompany/' className='flex justify-center items-center gap-2 hover:text-[#0A66C2]'>
+          <a href='https://www.linkedin.com/company/literary-society-bit-mesra/mycompany/' className='flex justify-center items-center gap-2 duration-100 hover:text-[#0A66C2]'>
             <div className='opacity-70 text-3xl md:text-4xl flex items-center justify-center'>
               <ion-icon name="logo-linkedin"></ion-icon>
             </div>
