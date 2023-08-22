cbb2cf6e5f81f912cbd616c73babf7354fad03d3
Apoorv <apoorvsharma2603@gmail.com>
2023-08-22 22:01:17 +0530

    Added Gallery Carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,64 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+import g1 from '../assets/gallery/g1.jpg'
+import g2 from '../assets/gallery/g2.jpg'
+import g3 from '../assets/gallery/g3.jpg'
+import g4 from '../assets/gallery/g4.jpg'
+import g5 from '../assets/gallery/g5.jpg'
+import g6 from '../assets/gallery/g6.jpg'
+import g7 from '../assets/gallery/g7.jpg'
+
+const Carousel = () => {
+  return (
+    <>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper max-w-[900px] pt-[50px] pb-[50px]"
+      >
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g1 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g2 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g3 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g4 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g5 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g6 } className='block w-full' />
+        </SwiperSlide>
+        <SwiperSlide className='bg-center bg-cover w-[300px] h-[200px] lg:w-[450px] lg:h-[300px]' >
+          <img src= { g7 } className='block w-full' />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  )
+}
+
+export default Carousel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import Carousel from '../components/Carousel'
+
 import About_bg from '../assets/about_bg.jpg'
 import Gallery_bg from '../assets/gallery_bg.jpg'
 
@@ -15,15 +18,8 @@ const Home = () => {
           <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
           <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
         </section>
-        <section id='gallery'>
-          <div className='w-fit h-fit'>
-            <h1 className='relative top-32 lg:top-40 bg-[#00000098] text-[#ccb57e] w-full text-center p-5 text-3xl z-50'>GALLERY</h1>
-            <div className='bg-[#302306]'>
-              <img src={Gallery_bg} alt="gallery" className='inset-0 min-w-[100vw] h-[200px] lg:h-auto max-h-[280px] object-cover object-center z-0 opacity-25'/>
-            </div>
-          </div>
-          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eos, voluptas aliquid accusantium laborum numquam nihil molestias maiores esse dignissimos? Eaque id obcaecati perspiciatis doloremque numquam dolorum fugiat quae error!</p>
-          <p className='w-[80vw] mt-10 m-auto text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus praesentium repellat exercitationem quisquam doloremque error dolor veniam, at ab ipsam fugiat quaerat odit quidem accusamus repellendus unde officiis esse?</p>
+        <section>
+          <Carousel />
         </section>
       </div>
   )
